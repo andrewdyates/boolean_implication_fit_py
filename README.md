b70887f398b3ceab620e3f984d579cd4fcf789e6
Andrew Yates <andrewyates.name@gmail.com>
2013-06-04 18:14:33 -0400

    example code

--- README.md
+++ README.md
@@ -2,3 +2,11 @@ boolean_implication_fit_py
 ==========================
 
 Optimized python implementation of stepfit and all-pairs boolean implication.
+
+EXAMPLE USE:
+  $ python script.py fname=nice.may3.Eg.expr.gold.celegans.csv b=0.3
+
+Class enumerations:
+  0:NA, 1:YX, 2:PC, 3:XY, 4:UNL, 5:MX, 6:NC, 7:OR
+  
+See __init__.py for library functions. 
--- script.py
+++ script.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python
+"""Compute all pairs boolean class.
+
+USE:
+python script.py fname=nice.may3.Eg.expr.gold.celegans.csv b=0.3
+"""
 import sys
 import matrix_io as mio
 from __init__ import *
@@ -7,6 +12,7 @@ import cPickle as pickle
 
 def main(fname=None, pkl=True, **kwds):
   assert fname
+  if isinstance(pkl, basestring) and pkl.lower() in ('f','false','none'): pkl = False
   if 'b' in kwds: kwds['b'] = float(kwds['b'])
   if 'z_th' in kwds: kwds['z_th'] = float(kwds['z_th'])
   if 'err_th' in kwds: kwds['err_th'] = float(kwds['err_th'])
@@ -15,10 +21,10 @@ def main(fname=None, pkl=True, **kwds):
   D = mio.load(fname)
   print "Computing all pairs boolean class..."
   CLS = compute_all(D['M'], **kwds)
-  print "Saving %s..." % fname+'.tab'
+  print "Saving %s..." % (fname+'.tab')
   mio.save(CLS, fname+'.tab', fmt="%d", row_ids=D['row_ids'], col_ids=D['row_ids'])
   if pkl:
-    print "Saving %s..." % fname+'.pkl'
+    print "Saving %s..." % (fname+'.pkl')
     pickle.dump(CLS, open(fname+".pkl","w"))
 
 if __name__ == "__main__":
